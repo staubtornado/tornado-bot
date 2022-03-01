d63bbc4526dcded219d1835081124089df423b58
staubtornado <76395253+staubtornado@users.noreply.github.com>
2022-03-01 13:06:38 +0100

    Made ExperienceSysten more flexible

--- cogs/experience.py
+++ cogs/experience.py
@@ -2,7 +2,7 @@ from asyncio import sleep
 from random import randint
 from sqlite3 import Cursor
 
-from discord import slash_command, ApplicationContext, Message, Bot, Embed
+from discord import slash_command, ApplicationContext, Message, Bot, Embed, Option, Member
 from discord.ext.commands import Cog
 
 from data.config.settings import SETTINGS
@@ -12,7 +12,7 @@ on_cooldown: list = []
 
 
 class ExperienceSystem:
-    def __init__(self, bot: Bot, message: Message):
+    def __init__(self, bot: Bot, message: ApplicationContext or Message):
         self.bot = bot
         self.message = message
 
@@ -24,43 +24,55 @@ class ExperienceSystem:
 
         self.xp = None
         self.level = None
+        self.messages = None
 
-    async def get_xp(self) -> int:
-        return -1
+        self._cur: Cursor = database.cursor()
 
-    async def start(self) -> bool:
-        if self.message.author.bot or self.message.guild is None:
-            return False
-
-        await self.add_xp()
-
-        return True
-
-    async def add_xp(self) -> None:
-        cur: Cursor = database.cursor()
-
-        cur.execute(f"""
-            SELECT XP, Level from experience where (GuildID, UserID) = (
+        self._cur.execute(f"""
+            SELECT XP, Level, Messages from experience where (GuildID, UserID) = (
                 {self.message.guild.id}, {self.message.author.id})
         """)
 
         try:
-            self.xp, self.level = cur.fetchone()
+            self.xp, self.level, self.messages = self._cur.fetchone()
         except TypeError:
-            cur.execute(f"""
+            self._cur.execute(f"""
                     INSERT INTO experience (GuildID, UserID) VALUES ({self.message.guild.id}, {self.message.author.id})
                 """)
             self.xp = 0
             self.level = 0
+            self.messages = 0
+            database.commit()
 
-        cur.execute(f"""UPDATE experience SET Messages = Messages + 1 
+    def get_xp(self) -> int:
+        return self.xp
+
+    def get_level(self) -> int:
+        return self.level
+
+    def get_messages(self) -> int:
+        return self.messages
+
+    async def start(self) -> bool:
+        if self.message.author.bot or self.message.guild is None:
+            return False
+        await self.add_xp()
+        return True
+
+    async def add_xp(self) -> None:
+        self._cur.execute(f"""
+            SELECT XP, Level from experience where (GuildID, UserID) = (
+                {self.message.guild.id}, {self.message.author.id})
+        """)
+
+        self._cur.execute(f"""UPDATE experience SET Messages = Messages + 1 
                         WHERE (GuildID, UserID) = ({self.message.guild.id}, {self.message.author.id})""")
         database.commit()
 
         if not (self.message.guild.id, self.message.author.id) in on_cooldown:
             self.xp += round(randint(self.min_xp, self.max_xp) * self.multiplier)
             await self.check_for_level_up()
-            cur.execute(f"""UPDATE experience SET (XP, Level) = ({self.xp}, {self.level})
+            self._cur.execute(f"""UPDATE experience SET (XP, Level) = ({self.xp}, {self.level})
                             WHERE (GuildID, UserID) = ({self.message.guild.id}, {self.message.author.id})""")
 
             database.commit()
@@ -68,8 +80,17 @@ class ExperienceSystem:
             await sleep(self.cooldown)
             on_cooldown.remove((self.message.guild.id, self.message.author.id))
 
-    def calc_xp(self) -> int:
-        return round(self.base_level * 1.1248 ** self.level)
+    def calc_xp(self, level=None) -> int:
+        if level is None:
+            level = self.level
+        return round(self.base_level * 1.1248 ** level)
+
+    def total_xp(self) -> int:
+        xp: int = 0
+
+        for i in range(self.level):
+            xp += self.calc_xp(level=i)
+        return xp + self.xp
 
     def progress_bar(self) -> str:
         percent = (self.xp / self.calc_xp()) * 100
@@ -97,21 +118,24 @@ class Experience(Cog):
         self.bot = bot
 
     @slash_command()
-    async def rank(self, ctx: ApplicationContext):
+    async def rank(self, ctx: ApplicationContext, user: Option(Member, "Look up stats about others.",
+                                                               required=False)):
         await ctx.defer()
+        ctx.author.id = ctx.author.id or user.id
+        system: ExperienceSystem = ExperienceSystem(self.bot, ctx)
 
         embed = Embed(colour=SETTINGS["Colours"]["Default"])
-        embed.add_field(name="Total XP", value="-")
-        embed.add_field(name="Level", value="-")
-        embed.add_field(name="Progress", value="-")
-        embed.set_author(name=ctx.author.name, url=ctx.author.avatar.url)
+        embed.add_field(name="Total XP", value=system.total_xp())
+        embed.add_field(name="Level", value=system.get_level())
+        embed.add_field(name="Messages", value=system.get_messages())
+        embed.add_field(name=f"Progress ({system.get_xp()}XP / {system.calc_xp()}XP)", value=system.progress_bar())
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar.url)
         await ctx.respond(embed=embed)
 
     @slash_command()
     async def leaderboard(self, ctx: ApplicationContext):
-        pass
-
-
+        await ctx.respond("The leaderboard is currently under development. Use **/**`rank` to look up stats about you "
+                          "or others.")
 
 
 def setup(bot):
