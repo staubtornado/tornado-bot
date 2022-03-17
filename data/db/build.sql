5277a5d06c86a4322bed76c89916808d652945e1
staubtornado <76395253+staubtornado@users.noreply.github.com>
2022-03-17 11:36:32 +0100

    Created table for ticket_settings

--- data/db/build.sql
+++ data/db/build.sql
@@ -12,3 +12,11 @@ CREATE TABLE IF NOT EXISTS guilds (
     HasPremium integer DEFAULT 0,
     HasBeta integer DEFAULT 0
 );
+
+CREATE TABLE IF NOT EXISTS ticket_settings (
+    GuildID integer PRIMARY KEY,
+    CreateVoiceChannel integer DEFAULT 0,
+    RoleID message_text,
+    CategoryID integer
+
+)
