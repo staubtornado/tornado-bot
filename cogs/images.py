4a0457c7a5c89670c176e7de958438c2ef35f28e
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-04-04 19:28:42 +0200

    Added command descriptions

--- cogs/images.py
+++ cogs/images.py
@@ -51,34 +51,42 @@ class Images(Cog):
 
     @slash_command()
     async def porn(self, ctx: ApplicationContext):
+        """Sends a porn image. Requires NSFW channel."""
         await self.send(ctx, "babe", "Here, take that porn!")
 
     @slash_command()
     async def teen(self, ctx: ApplicationContext):
+        """Sends a teen (>18) porn image. Requires NSFW channel."""
         await self.send(ctx, "teen", "Why is the FBI here?")
 
     @slash_command()
     async def ass(self, ctx: ApplicationContext):
+        """Sends an ass porn image. Requires NSFW channel."""
         await self.send(ctx, "ass", "Here, take some booty.")
 
     @slash_command()
     async def asian(self, ctx: ApplicationContext):
+        """Sends an asian porn image. Requires NSFW channel."""
         await self.send(ctx, "asian", "Asian hate does not exist.")
 
     @slash_command()
     async def masturbation(self, ctx: ApplicationContext):
+        """Sends a masturbation porn image. Requires NSFW channel."""
         await self.send(ctx, "masturbation", "Maybe the opposite of you?")
 
     @slash_command()
     async def shaved(self, ctx: ApplicationContext):
+        """Sends a shaved pussy image. Requires NSFW channel."""
         await self.send(ctx, "shaved", "That looks clean.")
         
     @slash_command()
     async def closeup(self, ctx: ApplicationContext):
+        """Sends a closeup image of a pussy. Requires NSFW channel."""
         await self.send(ctx, "close-up", "4K UHD Ultra High Quality")
 
     @slash_command()
     async def pussy(self, ctx: ApplicationContext):
+        """Sends a pussy image. Requires NSFW channel."""
         if random() > 0.3:
             await self.send(ctx, "pussy", "Take that pussy.")
             return
@@ -86,10 +94,12 @@ class Images(Cog):
 
     @slash_command()
     async def boob(self, ctx: ApplicationContext):
+        """Sends a boob image. Requires NSFW channel."""
         await self.send(ctx, "natural-tits", "Take that boobs.")
 
     @slash_command()
     async def milf(self, ctx: ApplicationContext):
+        """Sends a milf image. Requires NSFW channel."""
         await self.send(ctx, "milf", "Hey mom!")
 
 
