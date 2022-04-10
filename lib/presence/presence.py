b589e8c86ac90002b311f2d63221d801b09582cf
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-04-11 00:05:39 +0200

    Improved rich presence

--- lib/presence/presence.py
+++ lib/presence/presence.py
@@ -1,11 +1,13 @@
 from discord import Bot, Activity, ActivityType
 from discord.ext.tasks import loop
+from millify import millify
 
 from data.config.settings import SETTINGS
 
 
-@loop(minutes=30)
+@loop(minutes=SETTINGS["ServiceSyncInSeconds"])
 async def update_rich_presence(bot: Bot):
+    stats: str = f"{millify(len(bot.guilds), drop_nulls=True, precision=2)} servers | " \
+                 f"{millify(len(list(bot.get_all_members())), drop_nulls=True, precision=2)}"
     await bot.wait_until_ready()
-
-    await bot.change_presence(activity=Activity(type=ActivityType.playing, name=f"Version: {SETTINGS['Version']}"))
+    await bot.change_presence(activity=Activity(type=ActivityType.playing, name=SETTINGS['Version'] + f" {stats}"))
