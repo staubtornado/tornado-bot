d95c1a05645f988500128ca54059fa2f2465e3c8
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-04-27 19:49:17 +0200

    Fixed stats and improved style

--- lib/presence/presence.py
+++ lib/presence/presence.py
@@ -7,7 +7,7 @@ from data.config.settings import SETTINGS
 
 @loop(minutes=SETTINGS["ServiceSyncInSeconds"])
 async def update_rich_presence(bot: Bot):
-    stats: str = f"{millify(len(bot.guilds), drop_nulls=True, precision=2)} servers | " \
-                 f"{millify(len(list(bot.get_all_members())), drop_nulls=True, precision=2)}"
     await bot.wait_until_ready()
-    await bot.change_presence(activity=Activity(type=ActivityType.playing, name=SETTINGS['Version'] + f" {stats}"))
+    stats: str = f"{millify(len(bot.guilds), drop_nulls=True, precision=1)} servers | " \
+                 f"{millify(len(list(bot.get_all_members())), drop_nulls=True, precision=2)} users"
+    await bot.change_presence(activity=Activity(type=ActivityType.playing, name=SETTINGS['Version'] + f" | {stats}"))
