b682b8b0d4b4f32b5a2775483881ea2ef7ca3587
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-05-12 20:30:27 +0200

    Removed files due to restructure

--- lib/currency/wallstreet.py
+++
@@ -1,9 +0,0 @@
-from discord import Bot
-
-from data.config.settings import SETTINGS
-from lib.currency.wallet import Wallet
-
-
-class Wallstreet:
-    def __init__(self, bot: Bot):
-        self.wallet = Wallet(bot.get_user(SETTINGS["OwnerIDs"][0]))
