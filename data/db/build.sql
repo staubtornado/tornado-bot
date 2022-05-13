89328e9a75a7a7fa4e8b3e1cfdd707f6e134fb20
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-05-13 21:40:33 +0200

    Added subject table, refactored code, added fee to wallet

--- data/db/build.sql
+++ data/db/build.sql
@@ -23,8 +23,18 @@ CREATE TABLE IF NOT EXISTS wallets (
     UserID integer PRIMARY KEY,
     Balance integer DEFAULT 0,
     Revenue integer DEFAULT 0,
-    LastModified integer DEFAULT NULL,
-    GlobalTransactions integer DEFAULT 0
+    LastModified message_text DEFAULT NULL,
+    GlobalTransactions integer DEFAULT 0,
+    Fee REAL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS subjects (
+    GuildID integer,
+    Subject integer,
+    Seller integer,
+    Price integer,
+    Added message_text,
+    PRIMARY KEY (GuildID, Subject)
 );
 
 CREATE TABLE IF NOT EXISTS settings (
