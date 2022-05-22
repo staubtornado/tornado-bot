8f760166843b3b73422312030287912b14b56d9b
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-05-22 11:51:20 +0200

    Redesigned most of settings commands

--- cogs/settings.py
+++ cogs/settings.py
@@ -4,93 +4,108 @@ from discord.ext.commands import Cog
 from data.db.memory import database
 
 
+def values_valid(option: str, value: str) -> bool:
+    if " - " in option:  # True if options are an area of integers
+        values = option.split(": ")[1].replace("[", "").replace("]", "").split(" - ")
+        try:
+            value = float(value)
+            if value > int(values[1]) or value < int(values[0]):
+                raise ValueError
+        except ValueError:
+            return False
+        return True
+
+    values = option.split(": ")[1].replace("[", "").replace("]", "").lower().split(" | ")
+    if value in values:
+        return True
+    return False
+
+
 class Settings(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
+    async def cog_before_invoke(self, ctx: ApplicationContext):
+        database.cursor().execute("""INSERT OR IGNORE INTO settings (GuildID) VALUES (?)""", (ctx.guild.id,))
+
     settings = SlashCommandGroup("settings", "Change the bots settings on this server.",
                                  permissions=[CommandPermission("manage_guild", 2, True)])
 
-    music_settings = settings.create_subgroup("music", "Music settings.")
-    experience_settings = settings.create_subgroup("experience", "Experience settings.")
-    ticket_settings = settings.create_subgroup("tickets", "Ticket settings.")
+    ticket_settings = settings.create_subgroup("tickets_setup", "Ticket settings.")
 
-    @music_settings.command()
-    async def embed(self, ctx: ApplicationContext,
-                    size: Option(str, "Change the song embed size.", choices=["Large", "Medium", "Small"],
-                                 required=True)):
-        """Change the embed style of songs."""
-        await ctx.defer(ephemeral=True)
-        values = {"Large": 2, "Medium": 1, "Small": 0}
-
-        cur = database.cursor()
-        if cur.execute("""SELECT MusicEmbedSize FROM settings WHERE GuildID = ?""",
-                       (ctx.guild.id, )).fetchone() is None:
-            cur.execute("""INSERT INTO settings (GuildID, MusicEmbedSize) VALUES (?, ?)""",
-                        (ctx.guild.id, values[size]))
-        else:
-            cur.execute("""UPDATE settings SET MusicEmbedSize = ? WHERE GuildID = ?""", (values[size], ctx.guild.id))
-        self.bot.get_cog("Music").get_voice_state(ctx).embed_size = values[size]
-
-        await ctx.respond(f"✅ **Changed size** of song embeds **to {str(size).lower()}**.", ephemeral=True)
-
-    @experience_settings.command()
-    async def enabled(self, ctx: ApplicationContext, state: bool):
-        """Enable / disable the experience system."""
+    @settings.command()
+    async def music(self, ctx: ApplicationContext,
+                    option: Option(str, "Select an option.", choices=["embed size: [Small | Medium | Large]",
+                                                                      "delete embed when finished: [True | False]"],
+                                   required=True), value: Option(str, "Set a value.", required=True)):
+        """Configure the music player."""
         await ctx.defer(ephemeral=True)
 
-        cur = database.cursor()
-        if cur.execute("""SELECT ExpIsActivated FROM settings WHERE GuildID = ?""",
-                       (ctx.guild.id, )).fetchone() is None:
-            cur.execute("""INSERT INTO settings (GuildID, ExpIsActivated) VALUES (?, ?)""", (ctx.guild.id, int(state)))
-        else:
-            cur.execute("""UPDATE settings SET ExpIsActivated = ? WHERE GuildID = ?""", (int(state), ctx.guild.id))
-
-        response = "Disabled"
-        if state:
-            response = "Enabled"
-        await ctx.respond(f"✅ **{response}** the **experience system** on this server.", ephemeral=True)
-
-    @experience_settings.command()
-    async def multiplier(self, ctx: ApplicationContext, multiplier: str):
-        """Change the multiplier of gained XP per message."""
-        await ctx.defer(ephemeral=True)
-
-        try:
-            multiplier = float(multiplier)
-            if multiplier > 5 or not multiplier > 1:
-                raise ValueError
-        except ValueError:
-            await ctx.respond("❌ The **multiplier has to be between 0 and 5**.", ephemeral=True)
+        if not values_valid(option, value.lower()):
+            await ctx.respond(f"❌ **{value} is not valid** for {option.split(': ')[0]}.\n"
+                              "👉 **Valid options are** shown **in the brackets** behind the option.", ephemeral=True)
             return
 
-        cur = database.cursor()
-        if cur.execute("""SELECT ExpMultiplication FROM settings WHERE GuildID = ?""",
-                       (ctx.guild.id, )).fetchone() is None:
-            cur.execute("""INSERT INTO settings (GuildID, ExpMultiplication) VALUES (?, ?)""",
-                        (ctx.guild.id, multiplier))
-        else:
-            cur.execute("""UPDATE settings SET ExpMultiplication = ? WHERE GuildID = ?""",
-                        (multiplier, ctx.guild.id))
-        await ctx.respond(f"✅ The XP **multiplier** has been **set to** `{multiplier}`.")
+        options = {"embed size": """UPDATE settings SET MusicEmbedSize = (?) WHERE GuildID = ?""",
+                   "delete embed when finished": """UPDATE settings SET MusicDeleteEmbedAfterSong = (?) 
+                                                    WHERE GuildID = ?"""}
+        values = {"embed size": {"large": 2, "medium": 1, "small": 0},
+                  "delete embed when finished": {"true": 1, "false": 0}}
+        value = value.lower()
+        option = option.split(": ")[0]
 
-    @ticket_settings.command()
-    async def voice(self, ctx: ApplicationContext, setting: bool):
-        """Select if tickets should also have a voice channel."""
+        cur = database.cursor()
+        cur.execute(options[option], (values[option][value], ctx.guild_id))
+        self.bot.get_cog("Music").get_voice_state(ctx).__setattr__(options[option].replace(" ", "_"),
+                                                                   (values[option][value]))
+        await ctx.respond(f"✅ **{option}** has been **set to {value}**.", ephemeral=True)
+
+    @settings.command()
+    async def experience(self, ctx: ApplicationContext,
+                         option: Option(str, "Select an option.", choices=["enabled: [True | False]",
+                                                                           "multiplier: [1 - 5]"],
+                                        required=True), value: Option(str, "Set a value.", required=True)):
+        """Configure the leveling."""
         await ctx.defer(ephemeral=True)
 
+        if not values_valid(option, value.lower()):
+            await ctx.respond(f"❌ **{value} is not valid** for {option.split(': ')[0]}.\n"
+                              "👉 **Valid options are** shown **in the brackets** behind the option.", ephemeral=True)
+            return
+
+        options = {"enabled": """UPDATE settings SET ExpIsActivated = (?) WHERE GuildID = ?""",
+                   "multiplier": """UPDATE settings SET ExpMultiplication = (?) WHERE GuildID = ?"""}
+        values = {"enabled": {"true": 1, "false": 0}}
+
+        try:
+            values["multiplier"] = {value: float(value)}
+        except ValueError:
+            pass
+        option = option.split(": ")[0]
+
         cur = database.cursor()
-        if cur.execute("""SELECT TicketsCreateVoiceChannel FROM settings WHERE GuildID = ?""",
-                       (ctx.guild.id, )).fetchone() is None:
-            cur.execute("""INSERT INTO settings (GuildID, TicketsCreateVoiceChannel) VALUES (?, ?)""",
-                        (ctx.guild.id, int(setting)))
-        else:
-            cur.execute("""UPDATE settings SET TicketsCreateVoiceChannel = ? WHERE GuildID = ?""",
-                        (int(setting), ctx.guild.id))
-        response = "no longer"
-        if setting:
-            response = "now"
-        await ctx.respond(f"✅ New **tickets {response} have** a **voice channel**.")
+        cur.execute(options[option], (values[option][value], ctx.guild_id))
+        await ctx.respond(f"✅ **{option}** has been **set to {value}**.", ephemeral=True)
+
+    @settings.command()
+    async def tickets(self, ctx: ApplicationContext,
+                      option: Option(str, "Select an option.", choices=["voice channel: [True | False]"],
+                                     required=True), value: Option(str, "Set a value.", required=True)):
+        """Configure the ticket system."""
+        await ctx.defer(ephemeral=True)
+
+        if not values_valid(option, value.lower()):
+            await ctx.respond(f"❌ **{value} is not valid** for {option.split(': ')[0]}.\n"
+                              "👉 **Valid options are** shown **in the brackets** behind the option.", ephemeral=True)
+            return
+
+        options = {"voice channel": """UPDATE settings SET TicketsCreateVoiceChannel = ? WHERE GuildID = ?"""}
+        values = {"voice channel": {"true": 1, "false": 0}}
+        option = option.split(": ")[0]
+
+        cur = database.cursor()
+        cur.execute(options[option], (values[option][value], ctx.guild_id))
+        await ctx.respond(f"✅ **{option}** has been **set to {value}**.", ephemeral=True)
 
     @ticket_settings.command()
     async def category(self, ctx: ApplicationContext, category: CategoryChannel):
@@ -99,7 +114,7 @@ class Settings(Cog):
 
         cur = database.cursor()
         if cur.execute("""SELECT TicketsCreateVoiceChannel FROM settings WHERE GuildID = ?""",
-                       (ctx.guild.id, )).fetchone() is None:
+                       (ctx.guild.id,)).fetchone() is None:
             cur.execute("""INSERT INTO settings (GuildID, TicketsCreateVoiceChannel) VALUES (?, ?)""",
                         (ctx.guild.id, category.id))
         else:
