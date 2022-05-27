eb384792a073490c06baddc83aef982a697d8a95
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-05-27 23:12:47 +0200

    Added old default_search back

--- lib/music/extraction.py
+++ lib/music/extraction.py
@@ -25,7 +25,7 @@ class YTDLSource(PCMVolumeTransformer):
         'logtostderr': False,
         'quiet': True,
         'no_warnings': False,
-        'default_search': 'auto',
+        'default_search': 'ytsearch',
         'source_address': '0.0.0.0',
     }
 
