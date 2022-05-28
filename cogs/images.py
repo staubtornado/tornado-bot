17ba77e17c52fa8ce29bbba5fb58a06f9ac3687b
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-05-28 12:19:57 +0200

    Refactored code, will now get images from reddit

--- cogs/images.py
+++ cogs/images.py
@@ -1,54 +1,56 @@
-from asyncio import run
+from json import loads
 from random import choice, random
 from urllib.parse import urlparse
 
 from discord import Bot, slash_command, ApplicationContext, Embed, Option, AutocompleteContext
 from discord.ext.commands import Cog
+from requests import get, Response
 from tqdm import tqdm
 
 from data.config.settings import SETTINGS
-from lib.images.scraping import ImageScraping
 
 
-async def get_categories(ctx: AutocompleteContext) -> list:
+async def get_categories(ctx: AutocompleteContext = None) -> list:
     rtrn = []
 
-    if ctx.bot.get_channel(ctx.interaction.channel_id).is_nsfw():
-        rtrn.extend(["Porn", "Porn-Gif", "Teen", "Ass", "Asian", "Masturbation", "Shaved", "Close-Up", "Pussy",
-                     "Boobs", "Milf"])
-    rtrn.extend(["Meme"])
+    if ctx is None or ctx.bot.get_channel(ctx.interaction.channel_id).is_nsfw():
+        rtrn.extend(["porn", "ass", "asian", "masturbation", "shaved", "close-up", "pussy", "boobs", "milf"])
+    rtrn.extend(["meme"])
     return rtrn
 
 
 class Images(Cog):
     def __init__(self, bot: Bot):
-        self.gallery: dict = {}
         self.bot = bot
-        run(self.create_gallery())
 
-    async def create_gallery(self):
-        categories: list = ["babe", "teen", "ass", "asian", "masturbation", "shaved", "close-up", "pussy",
-                            "cat-pictures", "natural-tits", "milf", "meme", "porn-gif"]
+        self.categories = {"porn": "Pornhub", "ass": "ass", "asian": "AsiansGoneWild",
+                           "masturbation": "Fingering", "shaved": "shavedpussies", "close-up": "closeup",
+                           "pussy": "pussy", "cat": "cats", "boobs": "tits", "milf": "milf", "meme": "dankmemes"}
+        self.gallery = {}
+        for category in tqdm(self.categories, "Scraping image urls"):
+            for response in dict(loads(self.request(subreddit=self.categories[category]).text))["memes"]:
+                try:
+                    self.gallery[category]
+                except KeyError:
+                    self.gallery[category] = []
+                self.gallery[category].append(response)
 
-        self.gallery.clear()
-        for category in tqdm(categories, "Scraping image urls"):
-            url: str = f"https://www.pornpics.de/{category}/"
-            if category == "cat-pictures":
-                url = "https://www.rd.com/list/cat-pictures/"
-            if category == "meme":
-                url = "https://www.pinterest.de/Mcnicollke/meme-page/"
-            if category == "porn-gif":
-                url = "https://gifsex.blog/24-teen-sex-gifs.html"
-            self.gallery[category] = ImageScraping(url).get_all_images()
+    def request(self, subreddit: str = "dankmemes", count: int = 50) -> Response:
+        return get(f"https://meme-api.herokuapp.com/gimme/{subreddit}/{count}")
 
-    async def send(self, ctx: ApplicationContext, category: str, message: str, nsfw: bool = True):
+    async def send(self, ctx: ApplicationContext, category: str, nsfw: bool = True):
         if nsfw and not ctx.channel.is_nsfw():
             await ctx.respond("🔞 This command **requires** an **NSFW** channel.", ephemeral=True)
             return
         await ctx.defer()
 
-        url = choice(self.gallery[category])
-        embed: Embed = Embed(title=message, colour=SETTINGS["Colours"]["Default"])
+        content = choice(self.gallery[category])
+        title = content["title"]
+        source = content["postLink"]
+        url = content["url"]
+        votes = content["ups"]
+        embed: Embed = Embed(title=title, colour=SETTINGS["Colours"]["Default"],
+                             description=f"[Source]({source}) | {votes} votes")
         embed.set_image(url=url)
         embed.set_footer(text=f"Provided by {urlparse(url).netloc}")
         await ctx.respond(embed=embed)
@@ -59,23 +61,13 @@ class Images(Cog):
                                      autocomplete=get_categories)):
         """Select a category from wich the bot will send a random image."""
 
-        categories = {"Porn": ("babe", "Here, take that porn!"), "Teen": ("teen", "Why is the FBI here?"),
-                      "Ass": ("ass", "Here, take some booty."), "Asian": ("asian", "Asian hate does not exist."),
-                      "Masturbation": ("masturbation", "Maybe the opposite of you?"),
-                      "Shaved": ("shaved", "That looks clean."), "Close-Up": ("close-up", "4K UHD Ultra High Quality"),
-                      "Pussy": ("pussy", "Take that pussy."), "cat": ("cat-pictures", "Take that pussy."),
-                      "Boobs": ("natural-tits", "Take that boobs."), "Milf": ("milf", "Hey mom!"),
-                      "Meme": ("meme", "Here we go.", False), "Porn-Gif": ("porn-gif", "More frames? You're welcome.")}
+        categories = {"porn": True, "ass": True, "asian": True, "masturbation": True, "shaved": True, "close-up": True,
+                      "pussy": True, "cat": True, "boobs": True, "milf": True, "meme":  False}
 
-        if category == "Pussy":
+        if category == "pussy":
             if not random() > 0.3:
                 category = "cat"
-
-        try:
-            nsfw = categories[category][2]
-        except IndexError:
-            nsfw = True
-        await self.send(ctx, categories[category][0], categories[category][1], nsfw=nsfw)
+        await self.send(ctx, category, nsfw=categories[category])
 
 
 def setup(bot: Bot):
