435151c7fb14ca936926389bcbcd8cf148d04658
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-09-25 20:40:54 +0200

    Made most of Spotify integration async

--- lib/music/api.py
+++ lib/music/api.py
@@ -3,11 +3,26 @@ from typing import Union
 
 from lyricsgenius import Genius
 from lyricsgenius.types import Song
+from asyncspotify import Client, ClientCredentialsFlow, PlaylistTrack, FullAlbum, FullTrack
 from spotipy import Spotify, SpotifyClientCredentials
 
-sp: Spotify = Spotify(auth_manager=SpotifyClientCredentials(client_id=environ['SPOTIFY_CLIENT_ID'],
-                                                            client_secret=environ['SPOTIFY_CLIENT_SECRET']))
-genius = Genius(environ["LYRICS_FIND_ACCESS_TOKEN"])
+sp: Spotify
+auth: ClientCredentialsFlow
+genius: Genius
+
+
+def init_music_api():
+    global sp
+    global auth
+    global genius
+
+    sp = Spotify(auth_manager=SpotifyClientCredentials(client_id=environ['SPOTIFY_CLIENT_ID'],
+                                                       client_secret=environ['SPOTIFY_CLIENT_SECRET']))
+    auth = ClientCredentialsFlow(
+        client_id=environ['SPOTIFY_CLIENT_ID'],
+        client_secret=environ['SPOTIFY_CLIENT_SECRET']
+    )
+    genius = Genius(environ["LYRICS_FIND_ACCESS_TOKEN"])
 
 
 def search_on_spotify(search: str, pattern: str = "track,artist") -> tuple[list[str], list[str]]:
@@ -21,40 +36,39 @@ def search_on_spotify(search: str, pattern: str = "track,artist") -> tuple[list[
     return rtrn
 
 
-def get_track_name(track_id) -> list[str]:
-    meta: dict = sp.track(track_id)
-    name = meta["name"]
-    artist = meta["artists"][0]["name"]
-    return [f"{name} by {artist}"]
+async def get_track_name(track_id: str) -> str:
+    async with Client(auth) as asp:
+        meta: FullTrack = await asp.get_track(track_id)
+    return f"{meta.name} by {meta.artists[0].name}"
 
 
-def get_playlist_track_names(playlist_id) -> list[str]:
-    songs: list = []
-    meta: dict = sp.playlist(playlist_id)
-    for song in meta['tracks']['items']:
-        name = song["track"]["name"]
-        artist = song["track"]["artists"][0]["name"]
-        songs.append(f"{name} by {artist}")
+async def get_playlist_track_names(playlist_id: str) -> list[str]:
+    songs: list[str] = []
+
+    async with Client(auth) as asp:
+        meta: list[PlaylistTrack] = await asp.get_playlist_tracks(playlist_id)
+
+    for i in range(len(meta)):
+        track: PlaylistTrack = meta[i]
+        songs.append(f"{track.name} by {track.artists[0].name}")
     return songs
 
 
-def get_album_track_names(album_id) -> list[str]:
-    songs: list = []
-    meta: dict = sp.album(album_id)
-    for song in meta['tracks']['items']:
-        name = song["name"]
-        artist = song["artists"][0]["name"]
-        songs.append(f"{name} by {artist}")
-    return songs
+async def get_album_track_names(album_id: str) -> list[str]:
+    tracks: list[str] = []
+    async with Client(auth) as asp:
+        meta: FullAlbum = await asp.get_album(album_id)
+    for track in meta.tracks:
+        tracks.append(f"{track.name} by {track.artists[0].name}")
+    return tracks
 
 
-def get_artist_top_songs(artist_id) -> list[str]:
-    songs: list = []
-    meta: dict = sp.artist_top_tracks(artist_id, country='US')
-    for song in meta["tracks"][:10]:
-        name = song["name"]
-        artist = song["artists"][0]["name"]
-        songs.append(f"{name} by {artist}")
+async def get_artist_top_songs(artist_id: str) -> list[str]:
+    songs: list[str] = []
+    async with Client(auth) as asp:
+        meta: list[FullTrack] = await asp.get_artist_top_tracks(artist_id, market='US')
+    for entry in meta:
+        songs.append(f"{entry.name} by {entry.artists[0].name}")
     return songs
 
 
