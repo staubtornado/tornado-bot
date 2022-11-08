728ec020b35f6e0e0312367541aac211a14b9104
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-11-08 16:29:41 +0100

    Continued work on welcome message generation

--- lib/audit_log/welcome_message.py
+++ lib/audit_log/welcome_message.py
@@ -6,56 +6,72 @@ from discord import Member
 from easy_pil import Editor, Font
 from numpy import average
 
+from lib.utils.utils import ordinal
+
 
 async def generate_welcome_message(member: Member):
-    banner: bool = False
-
-    if member.banner is not None:
-        background: bytes = await member.banner.read()
-        banner = True
-    else:
-        try:
-            background: bytes = await member.avatar.read()
-        except AttributeError:
-            background: bytes = await member.default_avatar.read()
+    try:
+        background: bytes = await member.avatar.read()
+    except AttributeError:
+        background: bytes = await member.default_avatar.read()
 
     image: Image = Image.open(BytesIO(background))
     colors: list[Union[tuple[int, tuple[int, ...]], Any]] = image.getcolors(maxcolors=image.height * image.width)
 
-    if not banner:
-        r: list[int] = []
-        g: list[int] = []
-        b: list[int] = []
-        a: list[int] = []
+    r: list[int] = []
+    g: list[int] = []
+    b: list[int] = []
+    a: list[int] = []
 
-        for color in colors:
-            r.append(color[1][0])
-            g.append(color[1][1])
-            b.append(color[1][2])
-            a.append(color[1][3])
+    for _color in colors:
+        r.append(_color[1][0])
+        g.append(_color[1][1])
+        b.append(_color[1][2])
+        a.append(_color[1][3])
+    color: tuple[int, ...] = (
+        int(average(r, axis=0)),
+        int(average(g, axis=0)),
+        int(average(b, axis=0)),
+        int(average(a, axis=0))
+    )
 
-        image = Image.new(
-            mode="RGBA",
-            size=(600, 400),
-            color=(
-                int(average(r, axis=0)),
-                int(average(g, axis=0)),
-                int(average(b, axis=0)),
-                int(average(a, axis=0))
-            )
-        )
+    image = Image.new(
+        mode="RGBA",
+        size=(1100, 500),
+        color=color
+    )
 
-    if banner:
-        try:
-            background = await member.avatar.read()
-        except AttributeError:
-            background = await member.default_avatar.read()
     avatar: Editor = Editor(Image.open(BytesIO(background)))
-    avatar.resize((200, 200))
+    avatar.resize((256, 256))
     avatar.circle_image()
 
     editor: Editor = Editor(image)
     editor.rounded_corners()
-    editor.paste(avatar, (200, 50))
-    editor.text((100, 300), text=f"{member} joined the server!", font=Font(path="./assets/font.ttf", size=20))
-    editor.show()
+    editor.paste(avatar, (422, 50))
+    editor.ellipse(
+        position=(416, 44),
+        width=268,
+        height=268,
+        outline=(79, 84, 92),
+        stroke_width=7
+    )
+
+    text_color: tuple[int, int, int] = (255, 255, 255)
+    if (color[0] * 0.299 + color[1] * 0.587 + color[2] * 0.114) > 186:  # https://stackoverflow.com/a/3943023
+        text_color = (0, 0, 0)
+
+    editor.text(
+        position=(550, 376),
+        text=f"{member} joined the server!",
+        font=Font(path="./assets/font.ttf", size=40),
+        align="center",
+        color=text_color
+    )
+    editor.text(
+        position=(550, 423),
+        text=f"{ordinal(len(member.guild.members))} Member",
+        font=Font(path="./assets/font.ttf", size=32),
+        align="center",
+        color=text_color
+    )
+    editor.save("./test.png", format="PNG")
