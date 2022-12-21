f4b61313312dfdedc00ddeb02d0db0e643522a88
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2022-12-21 12:03:04 +0100

    Improved style of leaderboard

--- lib/experience/gen_leaderboard.py
+++ lib/experience/gen_leaderboard.py
@@ -9,29 +9,44 @@ from lib.db.data_objects import ExperienceStats
 from lib.utils.utils import shortened
 
 
-def _get_texts(stats: ExperienceStats) -> list[Text]:
-    return [
-        Text(
-            text="Total XP",
-            color=(255, 255, 255),
-            font=Font(path="./assets/font.ttf", size=27),
-        ),
-        Text(
-            text=str(shortened(stats.total)),
-            color=(236, 246, 19),
-            font=Font(path="./assets/font.ttf", size=27),
-        ),
-        Text(
-            text="      Level",
-            color=(255, 255, 255),
-            font=Font(path="./assets/font.ttf", size=27),
-        ),
-        Text(
-            text=str(stats.level),
-            color=(236, 246, 19),
-            font=Font(path="./assets/font.ttf", size=27),
-        )
-    ]
+def _add_row(editor: Editor, index: int, start_px: int, offset_px: int, stats: ExperienceStats) -> None:
+    editor.text(
+        text=f"{index}. {stats.member}",
+        color=(255, 255, 255),
+        font=Font(path="./assets/font.ttf", size=27),
+        position=(70, start_px + offset_px * (index - (8 if index > 7 else 1)))
+    )
+
+    editor.multi_text(
+        texts=[
+            Text(
+                text="Total XP",
+                color=(255, 255, 255),
+                font=Font(path="./assets/font.ttf", size=27),
+            ),
+            Text(
+                text=str(shortened(stats.total)),
+                color=(236, 246, 19),
+                font=Font(path="./assets/font.ttf", size=27),
+            )],
+        position=(562, start_px + offset_px * (index - (8 if index > 7 else 1))),
+        align="left"
+    )
+    editor.multi_text(
+        texts=[
+            Text(
+                text="Level",
+                color=(255, 255, 255),
+                font=Font(path="./assets/font.ttf", size=27),
+            ),
+            Text(
+                text=str(stats.level),
+                color=(236, 246, 19),
+                font=Font(path="./assets/font.ttf", size=27),
+            )],
+        position=(796, start_px + offset_px * (index - (8 if index > 7 else 1))),
+        align="left"
+    )
 
 
 async def generate_leaderboard_card(stats: list[ExperienceStats]) -> list[File]:
@@ -44,30 +59,15 @@ async def generate_leaderboard_card(stats: list[ExperienceStats]) -> list[File]:
     )
 
     items_per_column: int = 7
-    offset: int = 0
     for i, user_stats in enumerate(stats[:items_per_column], start=1):
         try:
             _avatar: bytes = await user_stats.member.avatar.read()
         except AttributeError:
             _avatar: bytes = await user_stats.member.default_avatar.read()
         avatar: Editor = Editor(Image.open(BytesIO(_avatar)).resize((30, 30))).circle_image()
-        editor.paste(avatar, position=(20, 220 + offset))
+        editor.paste(avatar, position=(20, 220 + 50 * (i - 1)))
+        _add_row(editor, i, 225, 50, user_stats)
 
-        editor.text(
-            text=f"{i}. {user_stats.member}",
-            color=(255, 255, 255),
-            font=Font(path="./assets/font.ttf", size=27),
-            position=(70, 225 + offset)
-        )
-
-        editor.multi_text(
-            texts=_get_texts(user_stats),
-            position=(875, 236 + offset),
-            align="right"
-        )
-        offset += 50
-
-    offset = 0
     editor2: Editor = Editor(Image.open("./assets/leaderboard2.png"))
     for i, user_stats in enumerate(stats[items_per_column:], start=items_per_column + 1):
         try:
@@ -75,21 +75,8 @@ async def generate_leaderboard_card(stats: list[ExperienceStats]) -> list[File]:
         except AttributeError:
             _avatar: bytes = await user_stats.member.default_avatar.read()
         avatar: Editor = Editor(Image.open(BytesIO(_avatar)).resize((30, 30))).circle_image()
-        editor2.paste(avatar, position=(20, 20 + offset))
-
-        editor2.multi_text(
-            texts=_get_texts(user_stats),
-            position=(875, 31 + offset),
-            align="right"
-        )
-
-        editor2.text(
-            text=f"{i}. {user_stats.member}",
-            color=(255, 255, 255),
-            font=Font(path="./assets/font.ttf", size=27),
-            position=(70, 20 + offset)
-        )
-        offset += 50
+        editor2.paste(avatar, position=(20, 15 + 50 * (i - items_per_column - 1)))
+        _add_row(editor2, i, 20, 50, user_stats)
 
     path: str = f"./data/cache/leaderboard{stats[0].member.guild.id}.png"
     editor.save(path, format="PNG")
