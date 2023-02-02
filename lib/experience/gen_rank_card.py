53a0fb07f0e8960cae02cac91af9addc34422d89
Staubtornado <76395253+Staubtornado@users.noreply.github.com>
2023-02-02 14:17:52 +0100

    Improved style of rank card

--- lib/experience/gen_rank_card.py
+++ lib/experience/gen_rank_card.py
@@ -12,12 +12,12 @@ from lib.utils.utils import shortened, read_file
 async def generate_rank_card(stats: ExperienceStats) -> File:
     editor: Editor = Editor(Image.new(
         mode="RGBA",
-        size=(934, 282),
+        size=(934, 251),
         color=(48, 51, 55, 255)
     ))
     editor.bar(
-        position=(476, 252),
-        max_width=442,
+        position=(468, 221),
+        max_width=446,
         height=19,
         percentage=round((stats.xp / level_size(stats.level))*100),
         color=(238, 136, 17)
@@ -61,28 +61,59 @@ async def generate_rank_card(stats: ExperienceStats) -> File:
         )
     ]
     editor.multi_text(
-        position=(840, 115),
+        position=(694, 100),
         texts=rank,
-        align="right",
+        space_separated=False
+    )
+
+    _percentile: float = (stats.rank / stats.member.guild.member_count) * 100
+    if _percentile <= 0.01:
+        percentile: tuple[str, str] = "TOP ", "  0.01"
+    elif _percentile <= 50:
+        percentile: tuple[str, str] = "TOP ", f"  {(round(_percentile, 2))}"
+    else:
+        percentile: tuple[str, str] = "BOTTOM ", f" {round(100 - _percentile, 2)}"
+    del _percentile
+
+    percentile: list[Text] = [
+        Text(
+            text=f"{percentile[0]}",
+            color=(163, 166, 170),
+            font=Font(path="./assets/fonts/Roboto-Regular.ttf", size=22)
+        ),
+        Text(
+            text=f"{percentile[1]}",
+            color=(238, 136, 17),
+            font=Font(path="./assets/fonts/Roboto-Regular.ttf", size=35)
+        ),
+        Text(
+            text=f"%",
+            color=(255, 255, 255),
+            font=Font(path="./assets/fonts/Roboto-Regular.ttf", size=35)
+        )
+    ]
+    editor.multi_text(
+        position=(694, 140),
+        texts=percentile,
         space_separated=False
     )
 
     messages: list[Text] = [
         Text(
-            text=f"MESSAGES",
-            color=(163, 166, 170),
-            font=Font(path="./assets/fonts/Roboto-Regular.ttf", size=22),
-        ),
-        Text(
-            text=f" {shortened(stats.message_amount)}",
+            text=f"{shortened(stats.message_amount)}",
             color=(238, 136, 17),
             font=Font(path="./assets/fonts/Roboto-Regular.ttf", size=35),
+        ),
+        Text(
+            text=f"MESSAGE{'S' if stats.message_amount != 1 else ''}",
+            color=(163, 166, 170),
+            font=Font(path="./assets/fonts/Roboto-Regular.ttf", size=22),
         )
     ]
     editor.multi_text(
-        position=(495, 115),
+        position=(661, 100),
         texts=messages,
-        align="left"
+        align="right"
     )
 
     level: list[Text] = [
@@ -98,13 +129,12 @@ async def generate_rank_card(stats: ExperienceStats) -> File:
         )
     ]
     editor.multi_text(
-        position=(495, 155),
-        texts=level,
-        align="left"
+        position=(475, 203),
+        texts=level
     )
 
     editor.text(
-        position=(910, 225),
+        position=(910, 194),
         text=f"{shortened(stats.xp)} / {shortened(level_size(stats.level))} XP",
         align="right",
         color=(255, 255, 255),
