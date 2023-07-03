3ae14543cea214f61d404ad7b6cf4f77123aa626
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-07-03 21:35:57 +0200

    Fixed issue in search options

--- lib/music/auto_complete.py
+++ lib/music/auto_complete.py
@@ -14,4 +14,4 @@ async def complete(ctx: CustomAutocompleteContext) -> list[str]:
 
     if not len(value) > 3:
         return results
-    return [f"{track.title} {track.artists[0]}" async for track in ctx.bot.spotify.search(value, limit=10)]
+    return [f"{track.title} {track.artists[0]}"[:100] async for track in ctx.bot.spotify.search(value, limit=10)]
