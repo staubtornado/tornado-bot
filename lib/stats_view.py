fc200e6b944a343ee4fd507aa34330fe861e2499
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-09-09 12:37:10 +0200

    Added stats to stats card

--- lib/stats_view.py
+++ lib/stats_view.py
@@ -1,8 +1,9 @@
+import time
 from asyncio import AbstractEventLoop, get_event_loop
 from io import BytesIO
 
 from discord import File, User, Member
-from easy_pil import Editor, Font
+from easy_pil import Editor, Font, Text
 
 from lib.db.db_classes import UserStats
 
@@ -37,7 +38,75 @@ def _generate_stats_card(
         position=(100, 50),
         image=Editor(BytesIO(avatar)).circle_image().resize((120, 120))
     )
+    editor.multi_text(
+        texts=[
+            Text(
+                text=str(commands_used),
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#ff3d87'
+            ),
+            Text(
+                text="commands used",
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#ffffff'
+            ),
+        ],
+        position=(450, 120),
+    )
 
+    hours = music_seconds_played // 3600
+    minutes = (music_seconds_played % 3600) // 60
+    editor.multi_text(
+        texts=[
+            Text(
+                text=f"{hours}",
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#ff3d87'
+            ),
+            Text(
+                text="hours",
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#ffffff'
+            ),
+            Text(
+                text="and",
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#808080'
+            ),
+            Text(
+                text=f"{minutes}",
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#ff3d87'
+            ),
+            Text(
+                text="minutes",
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#ffffff'
+            ),
+            Text(
+                text="music streamed",
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#808080'
+            ),
+        ],
+        position=(450, 170),
+    )
+
+    editor.multi_text(
+        texts=[
+            Text(
+                text=str(songs_played),
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#ff3d87'
+            ),
+            Text(
+                text="songs played",
+                font=Font('assets/fonts/Roboto/Roboto-Regular.ttf', size=25),
+                color='#ffffff'
+            ),
+        ],
+        position=(450, 220),
+    )
     return File(fp=editor.image_bytes, filename='stats.png')
 
 
