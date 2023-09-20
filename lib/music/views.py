9e7a2646014dcd5ed40af05d995b54e6cdaa2c49
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-09-20 18:44:37 +0200

    Added view for loop command

--- lib/music/views.py
+++ lib/music/views.py
@@ -2,6 +2,7 @@ from discord import Interaction, ButtonStyle
 from discord.ui import View, Button
 from lib.contexts import CustomApplicationContext
 from lib.db.db_classes import Emoji
+from lib.enums import AudioPlayerLoopMode
 from lib.music.audio_player import AudioPlayer
 
 from lib.spotify.track_collection import TrackCollection
@@ -55,6 +56,65 @@ class QueueFill(View):
             for button in self.children:
                 if isinstance(button, Button) and button.custom_id == interaction.custom_id:
                     self.value: str = button.label
+            emoji_checkmark: Emoji = await self.ctx.bot.database.get_emoji("checkmark")
+            await interaction.response.edit_message(
+                content=f"{emoji_checkmark} **You choose**: `{self.value}`",
+                view=None
+            )
+            self.stop()
+            return True
+        return False
+
+    async def on_check_failure(self, interaction: Interaction) -> None:
+        emoji_cross: Emoji = await self.ctx.bot.database.get_emoji("cross")
+        await interaction.response.send_message(
+            f'{emoji_cross} Only the command author can use this.',
+            ephemeral=True
+        )
+
+
+class LoopView(View):
+    def __init__(self, ctx: CustomApplicationContext, audio_player: AudioPlayer) -> None:
+        super().__init__(timeout=15)
+        self.ctx = ctx
+        self.audio_player = audio_player
+        self.value = None
+
+        self.add_item(
+            Button(
+                style=ButtonStyle.green,
+                label='Loop Queue',
+                custom_id=random_hex(8)
+            )
+        )
+        self.add_item(
+            Button(
+                style=ButtonStyle.green,
+                label='Loop Track',
+                custom_id=random_hex(8)
+            )
+        )
+        self.add_item(
+            Button(
+                style=ButtonStyle.red,
+                label='Cancel',
+                custom_id=random_hex(8)
+            )
+        )
+
+    async def interaction_check(self, interaction: Interaction) -> bool:
+        if interaction.user.id == self.ctx.author.id:
+            self.disable_all_items()
+
+            for button in self.children:
+                if isinstance(button, Button) and button.custom_id == interaction.custom_id:
+                    if button.label == 'Loop Queue':
+                        self.audio_player.loop = AudioPlayerLoopMode.QUEUE
+                    else:
+                        self.audio_player.loop = AudioPlayerLoopMode.SONG
+                    self.value: str = button.label
+                    break
+
             emoji_checkmark: Emoji = await self.ctx.bot.database.get_emoji("checkmark")
             await interaction.response.edit_message(
                 content=f"{emoji_checkmark} **You choose**: `{self.value}`",
