3fc16d3bd330c72967d1cb6e8317ee217136cba7
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-10-26 18:49:12 +0200

    Improved function of search complete

--- lib/music/auto_complete.py
+++ lib/music/auto_complete.py
@@ -1,6 +1,6 @@
 from urllib.parse import urlparse
+
 from lib.contexts import CustomAutocompleteContext
-from lib.spotify.data import SpotifyData
 
 
 async def complete(ctx: CustomAutocompleteContext) -> list[str]:
@@ -15,16 +15,14 @@ async def complete(ctx: CustomAutocompleteContext) -> list[str]:
     value = ctx.value
 
     if not len(value) > 3:
-        if ctx.command.name == "playnext":
+        if ctx.command.name in ("playnext", "search"):
             return []
 
-        playlists: list[SpotifyData] = await ctx.bot.spotify.get_trending_playlists()
-
-        for playlist in playlists:
+        for playlist in await ctx.bot.spotify.get_trending_playlists():
             results.append(f"Playlist: {playlist.name}")
         return results
     
-    # Check if value is any url
+    # Check if the value is any url
     if urlparse(value).scheme in ["http", "https"]:
         return []
     return [f"{track.name} {track.artists[0]}"[:100] async for track in ctx.bot.spotify.search(value, limit=10)]
