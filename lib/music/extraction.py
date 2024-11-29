72c0b3f0f8e37c8006c9fed9203f448a7283b254
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-11-29 16:57:29 +0100

    Remove oauth, as its no longer supported

--- lib/music/extraction.py
+++ lib/music/extraction.py
@@ -25,8 +25,7 @@ class YTDLSource(PCMVolumeTransformer):
         'source_address': '0.0.0.0',
         'default_search': 'auto',
         'playlist_items': '1,2,3,4,5',
-        'username': 'oauth2',
-        'password': 'p'
+
     }
 
     FFMPEG_OPTIONS = {
